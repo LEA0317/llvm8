4a8066b72043fdfbc965110e341155e88b5d88f8
Toshihiro KONDA <kontoshi0317@gmail.com>
2019-04-20 06:16:25 +0900

    modify test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  std::cout << "hello, world" << std::endl;
-
+  for (int i=0; i<argc;i++) {
+    std::cout << "hello, world1" << std::endl;
+    for (int j=10; j>i; j--) {
+      std::cout << "hello, world2" << std::endl;
+      for (int k=0; k<i; k++) {
+	std::cout << "hello, world3" << std::endl;
+      }
+    }
+    std::cout << "hello, world4" << std::endl;
+  }
   return 0;
 }
