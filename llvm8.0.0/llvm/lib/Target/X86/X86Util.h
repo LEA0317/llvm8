0fd8ee5f906b0150ba95b69671c3a8076b5f16a1
Toshihiro KONDA <kontoshi0317@gmail.com>
2019-04-20 10:02:58 +0900

    add utility functions

---
+++ llvm8.0.0/llvm/lib/Target/X86/X86Util.h
@@ -0,0 +1,69 @@
+#ifndef HEADER_X86_UTIL
+#define HEADER_X86_UTIL
+
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/CodeGen/MachineMemOperand.h"
+#include "llvm/CodeGen/TargetRegisterInfo.h"
+#include "llvm/PassAnalysisSupport.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+
+#include <iostream>
+
+#define B(X) (X->begin())
+#define E(X) (X->end())
+
+inline bool
+isBranchMI(llvm::MachineInstr* mi) {
+  if (mi->isBranch()) {
+    return true;
+  }
+  return false;
+}
+
+inline bool
+isConditionalBranchMI(llvm::MachineInstr* mi) {
+  if (mi->isBranch() && !mi->isBarrier() && !mi->isCall()) {
+    return true;
+  }
+  return false;
+}
+
+inline bool
+X86SimpleLoopAnalyzer(llvm::MachineBasicBlock *MBB) {
+  llvm::MachineBasicBlock::iterator MBBI;
+  llvm::MachineInstr               *BranchMI   = nullptr;
+  int                               num_branch = 0;
+
+  for (MBBI = B(MBB); MBBI != E(MBB); MBBI++) {
+    llvm::MachineInstr* MI = (llvm::MachineInstr*)&(*MBBI);
+    if (isBranchMI(MI)) {
+      BranchMI = MI;
+      num_branch++;
+
+      if (num_branch > 1) {
+	return false;
+      }
+    }
+  }
+
+  // one branch check
+  if (num_branch != 1) {
+    return false;
+  }
+
+  // loop structure check
+  if (isConditionalBranchMI(BranchMI)) {
+    llvm::MachineBasicBlock *TargetMBB = BranchMI->getOperand(0).getMBB();
+
+    if (TargetMBB->getNumber()      != MBB->getNumber()
+	|| TargetMBB->getFullName() != MBB->getFullName()) {
+      return false;
+    }
+  } else {
+    return false;
+  }
+
+  return true;
+}
+
+#endif
